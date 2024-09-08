e200d9c2cbdac10203de9901735c0f299d95706a
Harsimran Kaur <162671426+harsimrankaur2493@users.noreply.github.com>
2024-09-08 19:33:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoWrapper from './Components/TodoWrapper';
+import TodoForm from './Components/TodoForm';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper/>
+      
+    </div>
+  );
+}
+
+export default App;
